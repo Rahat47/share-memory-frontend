fd359217a8804022d258dddc876042051e8a2fe8
Rahat47 <djrayhan8@gmail.com>
2021-12-22 19:37:05 +0600

    added navbar and feed page

---
+++ src/components/PinDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PinDetail = () => {
+    return (
+        <div>
+            <h1>Pin Detail</h1>
+        </div>
+    );
+};
+
+export default PinDetail;
--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,7 +1,42 @@
-const Pins = () => {
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
+
+const Pins = ({ user }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
-        <div>
-            <h1>Pins</h1>
+        <div className='px-2 md:px-5 '>
+            <div className='bg-gray-50'>
+                <Navbar
+                    searchTerm={searchTerm}
+                    user={user}
+                    setSearchTerm={setSearchTerm}
+                />
+            </div>
+            <div className='h-full'>
+                <Routes>
+                    <Route path='/' element={<Feed />} />
+                    <Route path='/category/:categoryId' element={<Feed />} />
+                    <Route
+                        path='/pin-details/:pinId'
+                        element={<PinDetail user={user && user} />}
+                    />
+                    <Route
+                        path='/create-pin'
+                        element={<CreatePin user={user && user} />}
+                    />
+                    <Route
+                        path='/search'
+                        element={
+                            <Search
+                                searchTerm={searchTerm}
+                                setSearchTerm={setSearchTerm}
+                            />
+                        }
+                    />
+                </Routes>
+            </div>
         </div>
     );
 };
