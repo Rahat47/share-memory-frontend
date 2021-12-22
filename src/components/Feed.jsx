c5a9307aaebcc32069af3cacc08589654f7a025f
Rahat47 <djrayhan8@gmail.com>
2021-12-22 22:09:19 +0600

    working on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../services/client';
+import { MasonryLayout, Spinner } from '.';
+import { feedQuery, searchQuery } from '../utils/data';
 
 const Feed = () => {
-    return (
-        <div>
-            <h1>Feed</h1>
-        </div>
-    );
+    const [loading, setLoading] = useState(false);
+    const [pins, setPins] = useState(null);
+    console.log(pins);
+    const { categoryId } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+        if (categoryId) {
+            const query = searchQuery(categoryId);
+
+            client
+                .fetch(query)
+                .then(data => {
+                    setPins(data);
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.log(err);
+                    setLoading(false);
+                });
+        } else {
+            client
+                .fetch(feedQuery)
+                .then(data => {
+                    setPins(data);
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.log(err);
+                    setLoading(false);
+                });
+        }
+    }, [categoryId]);
+
+    if (loading)
+        return <Spinner message='We are adding new ideas to your feed.' />;
+
+    return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
--- src/utils/data.js
+++ src/utils/data.js
@@ -3,3 +3,55 @@ export const userQuery = (userId) => {
 
     return query
 }
+
+
+export const searchQuery = (searchTerm) => {
+    const query = `*[_type == "pin" && title match "${searchTerm}*" || category match "${searchTerm}*" || about match "${searchTerm}*"] {
+        image {
+            asset-> {
+                url
+            }
+        },
+        _id,
+        destination,
+        postedBy -> {
+            _id,
+            username,
+            image
+        },
+        save[] {
+            _key,
+            postedBy -> {
+                _id,
+                username,
+                image
+            },
+        },
+    }`
+
+    return query
+}
+
+// fetch all the available pins
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    image {
+        asset-> {
+            url
+        }
+    },
+    _id,
+    destination,
+    postedBy -> {
+        _id,
+        username,
+        image,
+    },
+    save[] {
+        _key,
+        postedBy -> {
+            _id,
+            username,
+            image
+        },
+    },
+}`
